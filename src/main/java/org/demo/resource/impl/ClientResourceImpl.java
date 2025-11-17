75d4fc7aac488f9459f1eb8717fbc71c1c6eefa6
Alejandro Torres Huaroma <atorrhua@emeal.nttdata.com>
2025-11-17 00:00:38 -0500

    Implementes metrics

--- src/main/java/org/demo/resource/impl/ClientResourceImpl.java
+++ src/main/java/org/demo/resource/impl/ClientResourceImpl.java
@@ -1,5 +1,8 @@
 package org.demo.resource.impl;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
@@ -18,24 +21,32 @@ import java.util.List;
 public class ClientResourceImpl implements ClientResource {
 
   @Override
+  @Counted(name = "clients_request", description = "Recuento de llamadas a GET /api/clients")
+  @Timed(name = "clients_duration", description = "Duración de llamada a GET /api/clients", unit = MetricUnits.MILLISECONDS)
   public List<Client> getAllClients() {
     Log.info("ClientResourceImpl -> Trayendo todos los clientes");
     return Client.listAll();
   }
 
   @Override
+  @Counted(name = "clients_by_id_request", description = "Recuento de llamadas a GET /api/clients/{id}")
+  @Timed(name = "clients_by_id_duration", description = "Duración de llamada a GET /api/clients/{id}", unit = MetricUnits.MILLISECONDS)
   public Client getClientById(Long id) {
     Log.info("ClientResourceImpl -> Trayendo client por id");
     return Client.findById(id);
   }
 
   @Override
+  @Counted(name = "clients_count_requests", description = "Recuento de llamadas a GET /count")
+  @Timed(name = "clients_count_duration", description = "Duración de llamada a GET /count", unit = MetricUnits.MILLISECONDS)
   public Long countAllClient() {
     Log.info("ClientResourceImpl -> Cantidad total de cliente");
     return Client.count();
   }
 
   @Override
+  @Counted(name = "post_client_request", description = "Recuento de llamadas a  POST /api/clients")
+  @Timed(name = "post_client_duration", description = "Duración de llamada a POST /api/clients}", unit = MetricUnits.MILLISECONDS)
   @Transactional
   public Response postClient(Client clientDto) {
     Log.info("ClientResourceImpl -> Creando cliente");
@@ -46,6 +57,8 @@ public class ClientResourceImpl implements ClientResource {
   }
 
   @Override
+  @Counted(name = "put_client_requests", description = "Recuento de llamadas a PUT /api/clients/{id}")
+  @Timed(name = "put_client_duration", description = "Duración de llamada a PUT /api/clients/{id}", unit = MetricUnits.MILLISECONDS)
   @Transactional
   public Client updateClient(Long id,Client client) {
     Log.info("ClientResourceImpl -> Actualizando cliente");
@@ -62,6 +75,8 @@ public class ClientResourceImpl implements ClientResource {
   }
 
   @Override
+  @Counted(name = "delete_client_requests", description = "Recuento de llamadas a DELETE /api/clients/{id}")
+  @Timed(name = "delete_client_duration", description = "Duración de llamada a DELETE /api/clients/{id}", unit = MetricUnits.MILLISECONDS)
   @Transactional
   public void deleteClient(Long id) {
     Log.info("ClientResourceImpl -> Eliminado cliente");
